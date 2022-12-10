0860134bdce154bc0d43ae476943d015170c422a
DeCorrean Davis <112592380+DeCorreanD@users.noreply.github.com>
2022-12-10 12:32:39 -0600

    OrderForm Almost Done

--- components/forms/addOrderForm.js
+++ components/forms/addOrderForm.js
@@ -1,5 +1,6 @@
 import renderToDom from '../../utils/renderToDom';
 import clearDom from '../../utils/clearDom';
+import selectOrderType from '../forms/selectOrderType'
 
 // Use this From for both Create/Update
 
@@ -8,20 +9,23 @@ const addOrderForm = (obj = {}) => {
 	const domString = `<form id="${obj.firebaseKey ? `edit-order--${obj.firebaseKey}` : "create-order"}" class="mb-4">
       <div class="form-group">
         <label for="image">Order Name</label>
-        <input type="text" class="form-control" id="first_name" placeholder="Order Name" value="${obj.first_name || ""}" required>
+        <input type="text" class="form-control" id="order_name" placeholder="Order Name" value="${obj.order_name || ""}" required>
       </div>
       <div class="form-group">
         <label for="image"></label>
-        <input type="text" class="form-control" id="last_name" placeholder="Last Name" value="${obj.last_name || ""}"required>
+        <input type="number" class="form-control" id="phone_number" placeholder="###-###-####" value="${obj.phone_number || ""}"required>
       </div>
       <div class="form-group">
         <label for="title">Email</label>
         <input type="email" class="form-control" id="email" aria-describedby="Email" placeholder="Enter Email" value="${obj.email || ""}"required>
       </div>
-      <button type="submit" class="btn btn-primary mt-3">Submit Author</button>
+      <div class="form-group" id="select-ordertype">
+      </div>
+      <button type="submit" class="btn btn-primary mt-3">Create/Edit Order</button>
     </form>`;
 
 	renderToDom("#form-container", domString);
+   selectOrderType(`${obj.order_type || ""}`);
 };
 
 export default addOrderForm;

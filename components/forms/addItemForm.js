1464c2bc814bc0c180626dbd6300be71020f9619
Evan Breland <evan.breland@gmail.com>
2022-12-10 13:28:35 -0600

    additemform

--- components/forms/addItemForm.js
+++ components/forms/addItemForm.js
@@ -11,13 +11,13 @@ const addItemForm = (obj = {}) => {
       </div>
       <div class="form-group">
       <label for="description">Description</label>
-      <textarea class="form-control" placeholder="Book Description" id="description" style="height: 100px">${obj.description || ''}</textarea>
+      <textarea class="form-control" placeholder="Book Description" id="description" style="height: 60px">${obj.description || ''}</textarea>
     </div>
       <div class="form-group">
       <label for="price">Item Price</label>
-      <textarea class="form-control" id="price" style="height: 40px">${obj.price || ''}</textarea>
+      <textarea type="number" class="form-control" id="price">${obj.price || ''}</textarea>
     </div>
-    <button type="submit" class="btn btn-primary">Submit Item
+    <button type="submit" class="btn btn-primary"> Create/Edit Item
     </button>
     </form>
     `;

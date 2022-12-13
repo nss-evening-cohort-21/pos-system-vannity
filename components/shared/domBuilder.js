a1ac928f68bc757792af5fbf59a1450a30057051
Van Hoang <111828642+vhoang11@users.noreply.github.com>
2022-12-12 19:34:39 -0600

    added home screen buttons

--- components/shared/domBuilder.js
+++ components/shared/domBuilder.js
@@ -5,7 +5,11 @@ const domBuilder = () => {
   <div id="navigation"></div>
   <div id="main-container">
     <div id="form-container"></div>
-    <div id="home-buttons"></div>
+    <div id="home-buttons">
+      <button id="view-orders">View Orders</button>
+      <button id="create-order">Create an Order</button>
+      <button id="view-revenue">View Revenue</button>
+    </div>
     <div id="store"></div>
     <div id="view"></div>
   </div>`;

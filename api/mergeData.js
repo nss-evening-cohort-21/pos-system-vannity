f524a681c91a78a3f9ed64fc1547e235782cfb9a
DeCorrean Davis <112592380+DeCorreanD@users.noreply.github.com>
2022-12-12 21:31:14 -0600

    ?

--- api/mergeData.js
+++ api/mergeData.js
@@ -1,12 +1,11 @@
 import { getSingleOrder, deleteOrder, getOrderMenuName } from './orderData';
-import { getSingleMenuName, deleteMenuName,  } from './menuData';
+import { getSingleMenuName, deleteMenuName } from './menuData';
 
 const getMenuNameDetails = (firebaseKey) => new Promise((resolve, reject) => {
   getSingleMenuName(firebaseKey).then((menuNameObject) => {
     getSingleOrder(menuNameObject.order_id)
       .then((orderObject) => resolve({ ...menuNameObject, orderObject }));
   }).catch(reject);
-
 });
 
 const getOrderDetails = (firebaseKey) => new Promise((resolve, reject) => {
@@ -17,14 +16,13 @@ const getOrderDetails = (firebaseKey) => new Promise((resolve, reject) => {
 });
 
 const deleteOrderMenuNameRelationship = (firebaseKey) => new Promise((resolve, reject) => {
-    getOrderMenuName(firebaseKey).then((orderMenuNameArray) => {
-      const deleteMenuNamePromises = orderMenuNameArray.map((menuName) => deleteMenuName(menuName.firebaseKey));
-  
-      Promise.all(deleteMenuNamePromises).then(() => {
-        deleteOrder(firebaseKey).then(resolve);
-      });
-    }).catch(reject);
-  });
-  
+  getOrderMenuName(firebaseKey).then((orderMenuNameArray) => {
+    const deleteMenuNamePromises = orderMenuNameArray.map((menuName) => deleteMenuName(menuName.firebaseKey));
 
-export { getOrderDetails, getMenuNameDetails, deleteOrderMenuNameRelationship }
+    Promise.all(deleteMenuNamePromises).then(() => {
+      deleteOrder(firebaseKey).then(resolve);
+    });
+  }).catch(reject);
+});
+
+export { getOrderDetails, getMenuNameDetails, deleteOrderMenuNameRelationship };

3fd4e961900b705735badb43cbddc57d4bf8226e
Van Hoang <111828642+vhoang11@users.noreply.github.com>
2022-12-13 19:10:19 -0600

    view orders page

--- components/events/navEvents.js
+++ components/events/navEvents.js
@@ -1,3 +1,5 @@
+import { getOrders } from '../../api/orderData';
+import { viewOrders, emptyOrders } from '../../pages/viewOrders';
 import { signOut } from '../../utils/auth';
 
 // navigation events
@@ -6,5 +8,15 @@ const navigationEvents = () => {
   document.querySelector('#logout-button')
     .addEventListener('click', signOut);
 };
+
+document.querySelector('#view-orders').addEventListener('click', () => {
+  getOrders().then((menuNameArray) => {
+    if (menuNameArray.length) {
+      viewOrders(menuNameArray);
+    } else {
+      emptyOrders();
+    }
+  });
+});
 
 export default navigationEvents;
---
+++ pages/viewOrders.js
@@ -0,0 +1,30 @@
+import clearDom from '../utils/clearDom';
+import renderToDOM from '../utils/renderToDom';
+
+const emptyOrders = () => {
+  const domString = '<h1>No Orders</h1>';
+  renderToDOM('#store', domString);
+};
+const viewOrders = (obj) => {
+  clearDom();
+
+  const domString = `
+  <div class="mt-5 d-flex flex-wrap">
+  <div class="d-flex flex-column">
+  <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${obj.order_name}</h5>
+    <h6 class="card-subtitle mb-2 text-muted">${obj.isFulfilled}</h6>
+    <p class="card-text">${obj.phone_number}</p>
+    <p class="card-text">${obj.email}</p>
+    <p class="card-text">${obj.order_type}</p>
+    <a href="#" id="view-details--${obj.firebaseKey}" class="card-link">Details</a>
+    <a href="#" id="edit-details--${obj.firebaseKey}"class="card-link">Edit</a>
+    <a href="#" id="delete-details--${obj.firebaseKey}"class="card-link">Delete</a>
+  </div>
+</div>`;
+
+  renderToDOM('#view', domString);
+};
+
+export { emptyOrders, viewOrders };

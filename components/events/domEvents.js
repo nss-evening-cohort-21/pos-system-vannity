3996bb07f32d038982b27e728582e7d76cbb81df
Van Hoang <111828642+vhoang11@users.noreply.github.com>
2022-12-15 19:10:15 -0600

    home buttons and navbar buttons working

--- components/events/domEvents.js
+++ components/events/domEvents.js
@@ -1,10 +1,22 @@
 import createOrderForm from '../forms/createOrderForm';
+import { getOrders } from '../../api/orderData';
+import { emptyOrders, viewOrders } from '../../pages/viewOrders';
 
 const domEvents = () => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
-    if (e.target.id.includes('create-order')) {
+    if (e.target.id.includes('home-create-order')) {
       createOrderForm();
     }
+  });
+
+  document.querySelector('#home-view-orders').addEventListener('click', () => {
+    getOrders().then((menuNameArray) => {
+      if (menuNameArray.length) {
+        viewOrders(menuNameArray);
+      } else {
+        emptyOrders();
+      }
+    });
   });
 };
 

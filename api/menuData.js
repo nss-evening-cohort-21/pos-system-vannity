e36209471ebcfdbfea8519e99ef6ed1aaf6ba38c
Van Hoang <111828642+vhoang11@users.noreply.github.com>
2022-12-17 19:20:52 -0600

    started adding user

--- api/menuData.js
+++ api/menuData.js
@@ -2,15 +2,21 @@ import client from '../utils/client';
 
 const endpoint = client.databaseURL;
 
-const getMenuNames = () => new Promise((resolve, reject) => {
-  fetch(`${endpoint}/menu.json`, {
+const getMenuNames = (uid) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/menu.json?orderBy="uid"&equalTo="${uid}"`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
     },
   })
     .then((response) => response.json())
-    .then((data) => resolve(Object.values(data)))
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
     .catch(reject);
 });
 

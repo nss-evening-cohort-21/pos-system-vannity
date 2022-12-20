89b4474a010493b861ebb386685d2002332ea2b7
Evan Breland <evan.breland@gmail.com>
2022-12-19 21:25:08 -0600

    item cards and updated details

---
+++ pages/itemCards.js
@@ -0,0 +1,22 @@
+import renderToDOM from '../utils/renderToDom';
+
+const itemCards = (itemArray) => {
+  let domString = '';
+  itemArray.forEach((item) => {
+    domString += `<div class="card">
+        
+              <div class="card-body" style="height: 300px;">
+                <h5 class="card-title">Item: ${item.name}</h5>
+                <p>Price:$ ${item.price}</p>
+              
+                <i id="edit-Item-btn--${item.firebaseKey}" class="logout-btn fas  btn btn-info"><i class="fas fa-edit"></i> Edit</i>
+                
+                <i id="delete-Item-btn--${item.firebaseKey}" class="logout-btn btn btn-danger fas"><i class="fas fa-trash-alt"></i> Delete</i>
+              </div>
+        
+            </div>`;
+  });
+  renderToDOM('#view', domString);
+};
+
+export default itemCards;
--- pages/viewOrderDetails.js
+++ pages/viewOrderDetails.js
@@ -1,19 +1,15 @@
 import clearDom from '../utils/clearDom';
 import renderToDOM from '../utils/renderToDom';
-import { getSingleOrder } from '../api/orderData';
+import { getMenuNames } from '../api/menuData';
+import itemCards from './itemCards';
 
-const viewOrderDetails = (firebaseKey) => {
+const viewOrderDetails = (order) => {
   clearDom();
-  // order ID, orders obj, order items with order
-  // putting data into a form
-  getSingleOrder(firebaseKey).then((obj) => {
-    // get single order with firebasekey
-    // make another firebase that will get order items
-    // add up the prices of the items
-    let domString = '';
 
-    domString += `
-      <h1>Order Name: ${obj.order_name}</h1>
+  let domString = '';
+
+  domString += `
+      <h1>Order Name: ${order.order_name}</h1>
     <h1>Order Total: </h1>
     <h2 class="dollarSign">$</h2>
     <div class="card">
@@ -22,27 +18,8 @@ const viewOrderDetails = (firebaseKey) => {
       <button class="btn btn-primary btn-lg mb-4" id="go-to-payment-btn">Go to Payment</button>'
       `;
 
-    const itemCards = (itemArray) => {
-      let domString = '';
-      itemArray.forEach((item) => {
-        domString += `<div class="card">
-          
-                <div class="card-body" style="height: 300px;">
-                  <h5 class="card-title">Item: ${item.name}</h5>
-                  <p>Price:$ ${item.price}</p>
-                
-                  
-                  <i id="edit-Item-btn--${item.firebaseKey}" class="logout-btn fas  btn btn-info"><i class="fas fa-edit"></i> Edit</i>
-                  
-                  <i id="delete-Item-btn--${item.firebaseKey}" class="logout-btn btn btn-danger fas"><i class="fas fa-trash-alt"></i> Delete</i>
-                </div>
-          
-              </div>`;
-      });
-      renderToDOM('#view', domString);
-    };
-    renderToDOM('#orderItems', domString);
-  });
+  renderToDOM('#form-container', domString);
+  getMenuNames().then(itemCards);
 };
 
 export default viewOrderDetails;

6baac9810e11188cf39cbe6a641cdd940755698c
Evan Breland <evan.breland@gmail.com>
2022-12-19 21:05:10 -0600

    orderdetails with item card

--- pages/viewOrderDetails.js
+++ pages/viewOrderDetails.js
@@ -1,31 +1,47 @@
 import clearDom from '../utils/clearDom';
-import renderToDom from '../utils/renderToDom';
+import renderToDOM from '../utils/renderToDom';
 import { getSingleOrder } from '../api/orderData';
 
-const viewOrderDetails = (firebaseKey, arr = []) => {
+const viewOrderDetails = (firebaseKey) => {
   clearDom();
+  // order ID, orders obj, order items with order
+  // putting data into a form
   getSingleOrder(firebaseKey).then((obj) => {
-    let cardString = '';
-    arr.map((item) => {
-      cardString += `
+    // get single order with firebasekey
+    // make another firebase that will get order items
+    // add up the prices of the items
+    let domString = '';
+
+    domString += `
       <h1>Order Name: ${obj.order_name}</h1>
     <h1>Order Total: </h1>
+    <h2 class="dollarSign">$</h2>
     <div class="card">
     <div class="card-body">
-      <h5 class="card-title">${item.name}</h5>
-      <p class="card-text">${item.price}</p>
-      ${obj.isFulfilled ? `
-      <a href="#" class="card-link" id="editItem--${item.firebaseKey}">Edit</a>
-      <a href="#" class="card-link" id="deleteItem--${item.firebaseKey}">Delete</a>` : ''}
-    </div>
-  </div>
-    `;
-      renderToDom('#orderItems', cardString);
+      '<button class="btn btn-success btn-lg mb-4" id="add-item-btn">Add Item</button>
+      <button class="btn btn-primary btn-lg mb-4" id="go-to-payment-btn">Go to Payment</button>'
+      `;
 
-      const btnString = '<button class="btn btn-success btn-lg mb-4" id="add-item-btn">Add Item</button><button class="btn btn-primary btn-lg mb-4" id="go-to-payment-btn">Go to Payment</button>';
-
-      renderToDom('#order-details-button', btnString);
-    });
+    const itemCards = (itemArray) => {
+      let domString = '';
+      itemArray.forEach((item) => {
+        domString += `<div class="card">
+          
+                <div class="card-body" style="height: 300px;">
+                  <h5 class="card-title">Item: ${item.name}</h5>
+                  <p>Price:$ ${item.price}</p>
+                
+                  
+                  <i id="edit-Item-btn--${item.firebaseKey}" class="logout-btn fas  btn btn-info"><i class="fas fa-edit"></i> Edit</i>
+                  
+                  <i id="delete-Item-btn--${item.firebaseKey}" class="logout-btn btn btn-danger fas"><i class="fas fa-trash-alt"></i> Delete</i>
+                </div>
+          
+              </div>`;
+      });
+      renderToDOM('#view', domString);
+    };
+    renderToDOM('#orderItems', domString);
   });
 };
 

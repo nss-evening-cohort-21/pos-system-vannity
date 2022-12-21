bd8fee599592fabf72d4b2d27fd3b529b6af12e8
Bruce Clark <bruce.clark55@gmail.com>
2022-12-21 08:59:42 -0600

    tip and revenue have been fixed

--- pages/viewRevenue.js
+++ pages/viewRevenue.js
@@ -3,14 +3,15 @@ import renderToDOM from '../utils/renderToDom';
 
 const viewRevenue = (array) => {
   clearDom();
-  const orderTotalArray = array.map((rev) => rev.order_total).reduce((total, newNumber) => total + newNumber, 0);
-  const tipAmountArray = array.map((rev) => rev.tip_amount).reduce((total, newNumber) => total + newNumber, 0);
+  const orderTotalArray = array.map((rev) => rev.order_total).reduce((total, newNumber) => Math.round(total + newNumber, 0));
+  const tipAmountArray = array.map((rev) => rev.tip_amount).reduce((total, newNumber) => Math.round(total + newNumber, 0));
   const domString = ` 
   <div class= "orderTotalArray">
    <div class= "card-body">
    <h1 class="card-title">Totals</h1>
    <h2 class="list-group-item"> Revenue Total: $${orderTotalArray}</2>
    <h2 class="list-group-item">Tip total: $${tipAmountArray}</2>
+
 
 
    </div>
